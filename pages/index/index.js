1fa18d78bf53b108a5f8c66d1694843991f0ec62
Kyouko2002 <200226126@qq.com>
2024-04-17 14:08:48 +0800

    Version 0.11 +js

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,10 +1,27 @@
 // index.js
 Page({
 data:{
+a:"通知公告"
 
 },
 aa(){
 
+},
+tabt1(){
+  this.setData({
+    a:"通知公告"
+  })
+
+},
+tabt2(){
+  this.setData({
+    a:"综合信息"
+  })
+},
+tabt3(){
+  this.setData({
+    a:"学术动态"
+  })
 },
 onLoad(options){
   
