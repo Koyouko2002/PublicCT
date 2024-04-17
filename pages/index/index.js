0c8c5d0df277c38bc5f76395d2af8984d6bc2681
Kyouko2002 <200226126@qq.com>
2024-04-17 16:10:36 +0800

    Version 0.11 add js part1

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,26 +1,39 @@
 // index.js
 Page({
 data:{
-a:"通知公告"
-
+a:"通知公告",
+is1:"block",
+is2:"none",
+is3:"none",
 },
 aa(){
 
 },
 tabt1(){
   this.setData({
-    a:"通知公告"
+    a:"通知公告",
+    is1:"block",
+    is2:"none",
+    is3:"none"
+
   })
 
 },
 tabt2(){
   this.setData({
-    a:"综合信息"
+    a:"综合信息",
+    is1:"none",
+    is2:"block",
+    is3:"none",
+
   })
 },
 tabt3(){
   this.setData({
-    a:"学术动态"
+    a:"学术动态",
+    is1:"none",
+    is2:"none",
+    is3:"block"
   })
 },
 onLoad(options){
