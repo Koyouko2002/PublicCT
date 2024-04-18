2a64de288a5c5f2887dada3e7f66fb043d23b107
Koyouko2002 <145365089+Koyouko2002@users.noreply.github.com>
2024-04-18 18:51:40 +0800

    L3
    +Buy 30/100

--- pages/buy/buy.js
+++ pages/buy/buy.js
@@ -5,9 +5,27 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    move1:"none",
+    move2:"block",
+    T1C:"white",
+    T2C:"black"
   },
-
+  Top1move(){
+   this.setData({
+    move1:"block",
+    move2:"none",
+    T1C:"black",
+    T2C:"white"
+   })
+  },
+  Top1move2(){
+    this.setData({
+      move1:"none",
+      move2:"block",
+      T1C:"white",
+      T2C:"black"
+     })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
