d09092ee5bc31e70f11cd81563df903c9309e0d2
Koyouko2002 <145365089+Koyouko2002@users.noreply.github.com>
2024-04-18 20:41:12 +0800

    L4
    Buy part 60/100

--- pages/buy/buy.js
+++ pages/buy/buy.js
@@ -8,8 +8,31 @@ Page({
     move1:"none",
     move2:"block",
     T1C:"white",
-    T2C:"black"
+    T2C:"black",
+    // order:{
+    //   id:1,
+    //   name:"紫气葡萄"
+    // },
+    arrOder:[{
+      name:"紫气葡萄"
+    },
+    {
+      name:"牧场嘻嘻乐"
+    },
+    {
+      name:"找真果茶"
+    },
+    { 
+      name:"招牌奶茶"
+    },
+  ],
+  isw1:"#ffffff",
+  isw2:"none",
+  isw2:"none",
+  isw3:"none",
+  isw4:"none",
   },
+
   Top1move(){
    this.setData({
     move1:"block",
@@ -26,6 +49,51 @@ Page({
       T2C:"black"
      })
   },
+  button1(){
+      this.setData({
+        isw1:"#ffffff",
+        isw2:"none",
+        isw3:"none",
+        isw4:"none",
+        isw5:"none",
+      })
+  },
+  button2(){
+    this.setData({
+      isw1:"none",
+      isw2:"#ffffff",
+      isw3:"none",
+      isw4:"none",
+      isw5:"none",
+    })
+},
+button3(){
+  this.setData({
+    isw1:"none",
+    isw2:"none",
+    isw3:"#ffffff",
+    isw4:"none",
+    isw5:"none",
+  })
+},
+button4(){
+  this.setData({
+    isw1:"none",
+    isw2:"none",
+    isw3:"none",
+    isw4:"#ffffff",
+    isw5:"none",
+  })
+},
+button5(){
+  this.setData({
+    isw1:"none",
+    isw2:"none",
+    isw3:"none",
+    isw4:"none",
+    isw5:"#ffffff",
+  })
+},
   /**
    * 生命周期函数--监听页面加载
    */
