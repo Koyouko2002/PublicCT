eb039b086c33ceef9ecf8299cb60040ce51f4951
夜倾 <1754994034@qq.com>
2024-04-20 01:02:34 +0800

    订单页面 前端功能完成

--- pages/order/order.js
+++ pages/order/order.js
@@ -5,32 +5,58 @@ Page({
    * 页面的初始数据
    */
   data: {
+    // 控制列表切换
+    activeId: 0,
+
+    // 控制订单切换
+    changeList: '',
+
     // 商品订单
-    orderList:[
+    orderList: [
       {
-        position:"千道茶坊绵阳城市学院安州店",
-        status:"已完成",
-        buyTime:"2024-04-09 18:22:54",
-        img:"/assets/Order/O1.png",
-        amount:14,
-        count:1
+        position: "千道茶坊绵阳城市学院安州店",
+        status: "已完成",
+        buyTime: "2024-04-09 18:22:54",
+        img: "/assets/Order/O1.png",
+        amount: 14,
+        count: 1
       },
       {
-        position:"千道茶坊绵阳城市学院安州店",
-        status:"已完成",
-        buyTime:"2024-04-09 18:22:54",
-        img:"/assets/Order/O1.png",
-        amount:14,
-        count:1
+        position: "千道茶坊绵阳城市学院安州店",
+        status: "已完成",
+        buyTime: "2024-04-09 18:22:54",
+        img: "/assets/Order/O1.png",
+        amount: 14,
+        count: 1
       },
+    ],
+
+    // 卡卷订单
+    cardList: []
+  },
+
+  // 列表切换
+  changeTab(e) {
+    // 获取传入id
+    let id = e.currentTarget.dataset.activeid;
+    let changeList=[
+      this.data.orderList,
+      this.data.cardList
     ]
+    this.setData({
+      activeId: id,
+      changeList:changeList[id]
+    })
+    console.log(e.currentTarget.dataset.activeid);
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-
+    this.setData({
+      changeList: this.data.orderList
+    })
   },
 
   /**
