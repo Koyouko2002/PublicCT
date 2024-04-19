c8e6613423cea99003c07b8d7df4f2aaa0619116
tong23316 <tong23316@qq.com>
2024-04-20 02:49:12 +0800

    T34添加跳转

--- pages/index/index.js
+++ pages/index/index.js
@@ -52,7 +52,7 @@ Page({
     const index = e.currentTarget.dataset.index;
     switch (index) {
       case 0:
-        wx.showToast({title: '敬请期待',icon: 'none',duration: 1000});
+        wx.navigateTo({ url: '/pages/mine/pandaShopping/pandaShopping' });
         break;
       case 1:
         wx.navigateTo({ url: '/pages/indexson/stucard/stucard' });
