5798a1898f1e22851a5f06e71c1f72c0c223a7f8
夜倾 <1754994034@qq.com>
2024-04-20 03:52:12 +0800

    主页根据是否登陆改变显示状态

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,7 +2,7 @@
 Page({
   data: {
     //小童，我加一点数据进去
-    userInfo:[],
+    userInfo: [],
 
 
     imgUrls: [
@@ -24,24 +24,24 @@ Page({
     ],
     duration2: 1200,         //动画时长
     items: [
-      {text: '熊猫币商城', mode:'/assets/熊猫币商城.png' },
-      {text: '学子卡', mode:'/assets/学子卡.png' },
-      {text: '会员任务', mode:'/assets/首页会员任务.png' },
-      {text: '悦己计划', mode:'/assets/悦己计划.png' }
+      { text: '熊猫币商城', mode: '/assets/熊猫币商城.png' },
+      { text: '学子卡', mode: '/assets/学子卡.png' },
+      { text: '会员任务', mode: '/assets/首页会员任务.png' },
+      { text: '悦己计划', mode: '/assets/悦己计划.png' }
     ],
   },
 
   // 小童我加个函数
   /* 页面加载监听 */
-onLoad(option){
-  // 得到缓存的用户登陆数据
-  let thisUserInfo = wx.getStorageSync('userInfo');
-  // console.log(thisUserInfo);
-  // 刷新渲染
-  this.setData({
-    userInfo:thisUserInfo
-  })
-},
+  onLoad(option) {
+    // 得到缓存的用户登陆数据
+    let thisUserInfo = wx.getStorageSync('userInfo');
+    // console.log(thisUserInfo);
+    // 刷新渲染
+    this.setData({
+      userInfo: thisUserInfo
+    })
+  },
 
 
   tabbuy: function () {
@@ -66,7 +66,7 @@ onLoad(option){
       duration: 1000
     });
   },
-  friTap: function(e) {
+  friTap: function (e) {
     const index = e.currentTarget.dataset.index;
     switch (index) {
       case 0:
@@ -76,7 +76,7 @@ onLoad(option){
         wx.navigateTo({ url: '/pages/indexson/stucard/stucard' });
         break;
       case 2:
-        wx.showToast({title: '请先登录',icon: 'none',duration: 1000});
+        wx.showToast({ title: '请先登录', icon: 'none', duration: 1000 });
         break;
       case 3:
         wx.navigateTo({ url: '/pages/indexson/friend/friend' });
@@ -85,5 +85,5 @@ onLoad(option){
         break;
     }
   }
-  
+
 })
