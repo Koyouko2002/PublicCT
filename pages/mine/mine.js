1a35579e46b3a133079540c47bd4774f0eb497d1
夜倾 <1754994034@qq.com>
2024-04-19 21:18:17 +0800

    个人中心静态页面完成
    移动个人中心相关静态资源
    完成个人中心静态页面

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -5,7 +5,64 @@ Page({
    * 页面的初始数据
    */
   data: {
+    //此处为进度条相关配置
+    progress:40,
+    color:'#07C160',
+    activeColor:'#fff',
+    borderRadius:'100',//圆角
+    strokeWidth:'9rpx',//进度条粗细
 
+    // 应用数据
+    apps:[
+      {
+        img:"/assets/mine/商城.png",
+        appDes:"熊猫币商城"
+      },
+      {
+        img:"/assets/mine/礼品卡.png",
+        appDes:"礼品卡"
+      },
+      {
+        img:"/assets/mine/券码兑换.png",
+        appDes:"券码兑换"
+      },
+      {
+        img:"/assets/mine/z.png",
+        appDes:"信息"
+      },
+      {
+        img:"/assets/mine/zz.png",
+        appDes:"悦己计划"
+      },
+      {
+        img:"/assets/mine/会员任务.png",
+        appDes:"会员任务"
+      },
+      {
+        img:"/assets/mine/福利.png",
+        appDes:"社区福利"
+      },
+      {
+        img:"/assets/mine/个人资料.png",
+        appDes:"个人资料"
+      },
+      {
+        img:"/assets/mine/加盟申请.png",
+        appDes:"加盟申请"
+      },
+      {
+        img:"/assets/mine/联系客服.png",
+        appDes:"联系客服"
+      },
+      {
+        img:"/assets/mine/我的勋章.png",
+        appDes:"我的勋章"
+      },
+      {
+        img:"/assets/mine/更多.png",
+        appDes:"更多"
+      },
+    ]
   },
 
   /**
