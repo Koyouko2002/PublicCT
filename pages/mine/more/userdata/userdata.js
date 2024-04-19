4f1db249eda07cab811a39d3701b14f3dd5fd5fb
zipikaka <2270299018@qq.com>
2024-04-20 03:02:54 +0800

    111

--- pages/mine/more/userdata/userdata.js
+++ pages/mine/more/userdata/userdata.js
@@ -5,6 +5,14 @@ Page({
    * 页面的初始数据
    */
   data: {
+    date:'2024-04-23'},
+
+  bindDateChange: function(e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      date: e.detail.value
+    })
+  
 
   },
 
