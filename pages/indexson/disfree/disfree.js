fff28b9fffe0c8fcddfc7b6145d3168a921fea39
tong23316 <tong23316@qq.com>
2024-04-19 22:31:24 +0800

    T26规则弹窗

--- pages/indexson/disfree/disfree.js
+++ pages/indexson/disfree/disfree.js
@@ -4,6 +4,17 @@ Page({
 
   data: {
     items2: ['/assets/元2无门槛.png', '/assets/元4无门槛.png', '/assets/元6无门槛.png', '/assets/元8无门槛.png'],
+    modalHidden: true,
   },
-
+  ifregister: function () {
+    var that = this;
+      that.setData({
+        modalHidden:false
+      })
+  },
+  confirmChange: function () {
+    this.setData({
+      modalHidden: true
+    })
+  },
 })
