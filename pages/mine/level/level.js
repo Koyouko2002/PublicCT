7eb4abf5045098f30dcc97bf2feeb89e9d534fd1
夜倾 <1754994034@qq.com>
2024-04-21 02:09:25 +0800

    等级界面进度条

--- pages/mine/level/level.js
+++ pages/mine/level/level.js
@@ -8,18 +8,24 @@ Page({
     userInfo: wx.getStorageSync('userInfo'),
 
     //此处为进度条相关配置
-    progress: 49,
+    progress: 25 * 3,
     color: '#07C160',
-    activeColor: '#fff',
+    activeColor: '#7395cd',
     borderRadius: '100',//圆角
     strokeWidth: '9rpx',//进度条粗细
+
+    icons: ["LV.0", "LV.1", "LV.2", "LV.3", "LV.4"]
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-
+    let userInfo = wx.getStorageSync('userInfo')
+    this.setData({
+      progress: userInfo.level * 25
+    })
   },
 
   /**
@@ -34,7 +40,7 @@ Page({
    */
   onShow() {
     this.setData({
-      userInfo:wx.getStorageSync('userInfo')
+      userInfo: wx.getStorageSync('userInfo')
     })
   },
 
