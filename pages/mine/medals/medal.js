cdd9a1acdb23509ff7d5bbea3ab7a8a2a298b13a
COCOVE1 <2781827036@qq.com>
2024-04-21 11:35:43 +0800

    徽章修改
    1

--- pages/mine/medals/medal.js
+++ pages/mine/medals/medal.js
@@ -10,12 +10,12 @@ Page({
         name:"活动限定徽章",
         kind1:[
           {            
-              pic:"/assets/mine/badge/pic (1).png",
-              txt1:"熊猫守卫者",
+              pic:"/assets/mine/badge/pic (9).png",
+              text1:"熊猫守卫者",
             },
             {
-              pic:"/assets/mine/badge/pic (2).png",
-              text1:"熊猫守卫者",
+              pic:"/assets/mine/badge/pic (12).png",
+              text1:"都市探索者",
             },
           
         ]
@@ -25,38 +25,79 @@ Page({
         name:"等级徽章",
         kind1:[
           {            
-              pic:"/assets/mine/badge/pic (1).png",
-              text1:"熊猫守卫者",
+              pic:"/assets/mine/badge/pic (8).png",
+              text1:"迷路徽章",
+            },
+            {
+              pic:"/assets/mine/badge/pic (11).png",
+              text1:"新生徽章",
+            },
+            {            
+              pic:"/assets/mine/badge/pic (6).png",
+              text1:"好奇徽章",
             },
             {
               pic:"/assets/mine/badge/pic (2).png",
-              text1:"熊猫守卫者",
+              text1:"饮茶徽章",
+            },
+            {            
+              pic:"/assets/mine/badge/pic (15).png",
+              text1:"茶艺徽章",
+            },
+            {
+              pic:"/assets/mine/badge/pic (5).png",
+              text1:"茶道徽章",
+            },
+            {            
+              pic:"/assets/mine/badge/pic (3).png",
+              text1:"黑金徽章",
             },
           
         ]
       },
       {
         name:"专属徽章",
+        kind1:[
+          {            
+              pic:"/assets/mine/badge/pic (9).png",
+              text1:"大茶道家",
+            },
+            {
+              pic:"/assets/mine/badge/pic (1).png",
+              text1:"天选黑金",
+            },
+          
+        ]
+
       },
       {
         name:"联名徽章",
+        kind1:[
+          {            
+              pic:"/assets/mine/badge/pic (4).png",
+              text1:"夏彦限定徽章",
+            },
+            {
+              pic:"/assets/mine/badge/pic (13).png",
+              text1:"左然限定徽章",
+            },
+            {
+              pic:"/assets/mine/badge/pic (7).png",
+              text1:"4人限定徽章",
+            },
+            {
+              pic:"/assets/mine/badge/pic (10).png",
+              text1:"莫奕限定徽章",
+            },
+            {
+              pic:"/assets/mine/badge/pic (14).png",
+              text1:"陆景河限定徽章",
+            },
+          
+        ]
+
       },
     ],
-    medal:
-    [
-      {
-      pic:"/assets/mine/badge/pic (1).png",
-      text1:"熊猫守卫者",
-    },
-    {
-      pic:"/assets/mine/badge/pic (2).png",
-      text1:"熊猫守卫者",
-    },
-  
-  
-  
-  ],
-
   },
 
   /**
