76344d938809c5e96e2ed1f60f6ceebc1266317b
tong23316 <tong23316@qq.com>
2024-04-22 00:10:33 +0800

    T66

--- pages/indexson/joingroup/joingroup.js
+++ pages/indexson/joingroup/joingroup.js
@@ -1,4 +1,6 @@
 // pages/indexson/joingroup/joingroup.js
+var open=0;
+var store="千道茶坊绵阳涪城区店";
 Page({
 
   /**
@@ -11,4 +13,17 @@ Page({
       url3:"/assets/下.png", 
       text2:"千道茶坊绵阳城市学院店", 
   },
+
+  explanTop: function () {
+      wx.navigateTo({ url: '/pages/buy/buyson/explan/explan' });
+  },
+
+  onReady() {
+    store=wx.getStorageSync('Store')
+    this.setData({
+      store:store
+    })
+  },
+
 })
+
