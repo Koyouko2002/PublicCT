9590fa655ff7f8cca0dbad4d485b2a3665ffd0cf
Kyouko2002 <200226126@qq.com>
2024-04-21 23:37:48 +0800

    登录3 的js部分

---
+++ pages/login3/login3.js
@@ -0,0 +1,84 @@
+// pages/login3/login3.js
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+  yj(){
+    wx.showModal({
+      title: '错误',
+      content: '点击登录以继续',
+      success: function (res) {
+        if (res.confirm) {//这里是点击了确定以后
+          console.log('用户点击确定')
+        } else {//这里是点击了取消以后
+          console.log('用户点击取消')
+        }
+      }
+    })  
+},
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+    wx.showToast({
+      title: '成功',
+      icon: 'success',
+      duration: 1000//持续的时间
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
